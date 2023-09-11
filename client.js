538545e6bae49441dc2522fc9f17f19e8cd56b04
Ben Rehmann <brehmann@me.com>
2023-09-11 18:30:24 -0500

    ready to manipulate

---
+++ client.js
@@ -0,0 +1,598 @@
+console.log(`Let the secrets begin!`);
+
+let allRecipes = [
+    // Soup - broccoli cheese
+    {
+        title: `Broccoli Cheese Soup`,
+        meal: `lunch or dinner`,
+        type: `soup`,
+        serving: `5 cups`,
+        ingredients: [
+                    {
+                        qty: 1,
+                        measurement: `cup`,
+                        item: `shell pasta`
+                    },
+                    {
+                        qty: 2,
+                        measurement: `cup`,
+                        item: `broccoli`
+                    },
+                    {
+                        qty: 0.5,
+                        measurement: `cup`,
+                        item: `onion`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tbs`,
+                        item: `butter`
+                    },
+                    {
+                        qty: 4,
+                        measurement: `tsp`,
+                        item: `chicken bouillon powder`
+                    },
+                    {
+                        qty: 0.25,
+                        measurement: `tsp`,
+                        item: `salt`
+                    },
+                    {
+                        qty: 0.08,
+                        measurement: `tsp`,
+                        item: `pepper`
+                    },
+                    {
+                        qty: 6,
+                        measurement: `pcs`,
+                        item: `mozzarella cheese`
+                    },
+                    {
+                        qty: 4,
+                        measurement: `pcs`,
+                        item: `broccoli florets`
+                    },
+                    ],
+        steps: [
+                {
+                    step1: `Boil pasta. Drain.`
+                },
+                {
+                    step2: `Steam broccoli.`
+                },
+                {
+                    step3: `Saute onion with the butter until soft.`
+                },
+                {
+                    step4: `Add to blender: 1 cup water, bouillon powder, broccoli an onion mixture. Blend.`
+                },
+                {
+                    step5: `Add blended mixture to pasta.`
+                },
+                {
+                    step6: `Add 2-1/2 cups of water, salt and pepper to mixture. Stir occasionally.`
+                },
+                {
+                    step7: `Divide the cheese among the bowls. Add soup. Garnish.`
+                },
+        ]
+    },
+
+    // Dessert - taffy apple salad
+    {
+        title: `Taffy Apple Salad`,
+        meal: `dessert`,
+        type: `fruity`,
+        serving: `feeds many`,
+        ingredients: [
+                    {
+                        qty: 1,
+                        measurement: `can`,
+                        item: `crushed pineapple`
+                    },
+                    {
+                        qty: 2,
+                        measurement: `cup`,
+                        item: `mini marshmallow`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tbs`,
+                        item: `flour`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tsp`,
+                        item: `vinegar`
+                    },
+                    {
+                        qty: 0.5,
+                        measurement: `cup`,
+                        item: `sugar`
+                    },
+                    {
+                        qty: 1,
+                        measurement: ``,
+                        item: `egg`
+                    },
+                    {
+                        qty: 8,
+                        measurement: `oz`,
+                        item: `cool whip`
+                    },
+                    {
+                        qty: 1.5,
+                        measurement: `cup`,
+                        item: `unsalted dry roasted peanuts`
+                    },
+                    {
+                        qty: 4,
+                        measurement: ``,
+                        item: `apples`
+                    },
+                    ],
+        steps: [
+                {
+                    step1: `Day 1`
+                },
+                {
+                    step2: `Drain pineapple can.`
+                },
+                {
+                    step3: `Mix pineapple and marshmallows.`
+                },
+                {
+                    step4: `Combine: flour, vinegar, sugar, egg and pineapple juice in a saucepan. Heat and stir until pudding like.`
+                },
+                {
+                    step5: `Day 2`
+                },
+                {
+                    step6: `Combine everything, mix in cool whip, nuts and apples.`
+                },
+                {
+                    step7: `Let sit 8-10 hrs, then enjoy!`
+                },
+                ]
+    },
+
+// ************ BREAKFAST CATEGORY ******************
+
+// Waffle - Bens Basic
+    {
+        title: `Ben's Basic Waffle`,
+        meal: `breakfast`,
+        type: `waffle`,
+        serving: `makes many`,
+        ingredients: [
+                    {
+                        qty: 2,
+                        measurement: `cup`,
+                        item: `flour`
+                    },
+                    {
+                        qty: 0.75,
+                        measurement: `cup`,
+                        item: `sugar`
+                    },
+                    {
+                        qty: 3.5,
+                        measurement: `tsp`,
+                        item: `baking powder`
+                    },
+                    {
+                        qty: 2,
+                        measurement: ``,
+                        item: `egg`
+                    },
+                    {
+                        qty: 1.5,
+                        measurement: `cup`,
+                        item: `milk`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tsp`,
+                        item: `vanilla`
+                    },
+                    {
+                        qty: 0.75,
+                        measurement: `cup`,
+                        item: `butter`
+                    },
+                    ],
+        steps: [
+                {
+                    step1: `Combine and mix all dry ingredients.`
+                },
+                {
+                    step2: `Combine wet ingredients into the dry ones.`
+                },
+                {
+                    step3: `Mix. Batter is complete.`
+                },
+                ]
+    },
+
+// Muffin - Cinnamon Banana
+    {
+        title: `Cinnamon Banana Muffins`,
+        meal: `breakfast`,
+        type: `muffin`,
+        serving: `makes many`,
+        ingredients: [
+                    {
+                        qty: 1.5,
+                        measurement: `cup`,
+                        item: `flour`
+                    },
+                    {
+                        qty: 0.25,
+                        measurement: `cup`,
+                        item: `brown sugar`
+                    },
+                    {
+                        qty: 0.25,
+                        measurement: `cup`,
+                        item: `sugar`
+                    },
+                    {
+                        qty: 0.25,
+                        measurement: `cup`,
+                        item: `flax seed`
+                    },
+                    {
+                        qty: 0.75,
+                        measurement: `tsp`,
+                        item: `baking soda`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tsp`,
+                        item: `cinnamon`
+                    },
+                    {
+                        qty: 0.5,
+                        measurement: `tsp`,
+                        item: `salt`
+                    },
+                    {
+                        qty: 3,
+                        measurement: `pcs`,
+                        item: `mashed banana`
+                    },
+                    {
+                        qty: 2,
+                        measurement: ``,
+                        item: `egg`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tsp`,
+                        item: `vanilla`
+                    },
+                    {
+                        qty: 0.33,
+                        measurement: `cup`,
+                        item: `yogurt`
+                    },
+                    {
+                        qty: 5,
+                        measurement: `tbs`,
+                        item: `butter`
+                    },
+                    ],
+        steps: [
+                {
+                    step1: `Combine and mix all dry ingredients.`
+                },
+                {
+                    step2: `Combine wet ingredients into the dry ones.`
+                },
+                {
+                    step3: `Mix batter.`
+                },
+                {
+                    step3: `Bake at 400* for 20 minutes.`
+                },
+                ]
+    },
+
+// Overnight Oats - Pumpkin Pie 
+    {
+        title: `Pumpkin Pie Overnight Oats`,
+        meal: `breakfast`,
+        type: `oatmeal`,
+        serving: `makes many`,
+        ingredients: [
+                    {
+                        qty: 2,
+                        measurement: `cup`,
+                        item: `oats`
+                    },
+                    {
+                        qty: 2,
+                        measurement: `cup`,
+                        item: `milk`
+                    },
+                    {
+                        qty: 0.5,
+                        measurement: `cup`,
+                        item: `pumpkin`
+                    },
+                    {
+                        qty: 2,
+                        measurement: `tbs`,
+                        item: `chia seeds`
+                    },
+                    {
+                        qty: 3,
+                        measurement: `tsp`,
+                        item: `pumpkin spice`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tsp`,
+                        item: `vanilla`
+                    },
+                    {
+                        qty: 2,
+                        measurement: `tbs`,
+                        item: `maple syrup`
+                    },
+                    {
+                        qty: 2,
+                        measurement: `tbs`,
+                        item: `maple syrup`
+                    },
+                    {
+                        qty: `to preference`,
+                        measurement: ``,
+                        item: `pecans`
+                    },
+                    ],
+        steps: [
+                {
+                    step1: `Combine and mix all ingredients.`
+                },
+                {
+                    step2: `Chill overnight. Enjoy the next morning!`
+                },
+                ]
+    },
+
+// Overnight Oats - Key Lime Pie
+    {
+        title: `Key Lime Pie Overnight Oats`,
+        meal: `breakfast`,
+        type: `oatmeal`,
+        serving: `makes many`,
+        ingredients: [
+                    {
+                        qty: 2,
+                        measurement: `cup`,
+                        item: `oats`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `cup`,
+                        item: `milk`
+                    },
+                    {
+                        qty: 3,
+                        measurement: `zest/juice`,
+                        item: `limes`
+                    },
+                    {
+                        qty: 2,
+                        measurement: `tbs`,
+                        item: `chia seeds`
+                    },
+                    {
+                        qty: 2,
+                        measurement: `tbs`,
+                        item: `honey`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `pinch`,
+                        item: `salt`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `cup`,
+                        item: `water`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `cup`,
+                        item: `coconut or lime yogurt`
+                    },
+                    ],
+        steps: [
+                {
+                    step1: `Combine and mix all ingredients.`
+                },
+                {
+                    step2: `Chill overnight. Enjoy the next morning!`
+                },
+                ]
+    },
+
+// Pancakes - Pumpkin
+    {
+        title: `Ben's Basic Waffle`,
+        meal: `breakfast`,
+        type: `pancake`,
+        serving: `makes many`,
+        ingredients: [
+                    {
+                        qty: 2,
+                        measurement: `cup`,
+                        item: `flour`
+                    },
+                    {
+                        qty: 0.25,
+                        measurement: `cup`,
+                        item: `sugar`
+                    },
+                    {
+                        qty: 0.25,
+                        measurement: `cup`,
+                        item: `brown sugar`
+                    },
+                    {
+                        qty: 3,
+                        measurement: `tsp`,
+                        item: `baking powder`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tsp`,
+                        item: `salt`
+                    },
+                    {
+                        qty: 1.5,
+                        measurement: `tsp`,
+                        item: `cinnamon`
+                    },
+                    {
+                        qty: 0.5,
+                        measurement: `tsp`,
+                        item: `ginger`
+                    },
+                    {
+                        qty: 0.5,
+                        measurement: `tsp`,
+                        item: `nutmeg`
+                    },
+                    {
+                        qty: 0.12,
+                        measurement: `tsp`,
+                        item: `cloves`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `cup`,
+                        item: `pumpkin`
+                    },
+                    {
+                        qty: 2,
+                        measurement: ``,
+                        item: `egg`
+                    },
+                    {
+                        qty: 1.5,
+                        measurement: `cup`,
+                        item: `milk`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tsp`,
+                        item: `vanilla`
+                    },
+                    {
+                        qty: 4,
+                        measurement: `tbs`,
+                        item: `butter`
+                    },
+                    ],
+        steps: [
+                {
+                    step1: `Combine and mix all dry ingredients.`
+                },
+                {
+                    step2: `Combine wet ingredients into the dry ones.`
+                },
+                {
+                    step3: `Mix. Batter is complete.`
+                },
+                ]
+    },
+
+// *********************** SAUCES **************************
+
+// BBQ Sauce - North Carolina
+    {
+        title: `North Carolina BBQ`,
+        meal: ``,
+        type: `sauce`,
+        serving: `good amount`,
+        ingredients: [
+                    {
+                        qty: 1,
+                        measurement: `cup`,
+                        item: `mustard`
+                    },
+                    {
+                        qty: 0.33,
+                        measurement: `cup`,
+                        item: `honey`
+                    },
+                    {
+                        qty: 0.33,
+                        measurement: `cup`,
+                        item: `apple cider vinegar`
+                    },
+                    {
+                        qty: 0.25,
+                        measurement: `cup`,
+                        item: `brown sugar`
+                    },
+                    {
+                        qty: 2,
+                        measurement: `tbs`,
+                        item: `ketchup`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tbs`,
+                        item: `worcestshire`
+                    },
+                    {
+                        qty: 1,
+                        measurement: `tsp`,
+                        item: `garlic powder`
+                    },
+                    {
+                        qty: 0.5,
+                        measurement: `tsp`,
+                        item: `hot sauce`
+                    },
+                    {
+                        qty: 0.5,
+                        measurement: `tsp`,
+                        item: `pepper`
+                    },
+                    {
+                        qty: 0.5,
+                        measurement: `tsp`,
+                        item: `onion powder`
+                    },
+                    {
+                        qty: 0.5,
+                        measurement: `tsp`,
+                        item: `salt`
+                    },
+                    {
+                        qty: 0.25,
+                        measurement: `tsp`,
+                        item: `chili powder`
+                    },
+                    {
+                        qty: 0.25,
+                        measurement: `tsp`,
+                        item: `cayenne pepper`
+                    },
+                    ],
+        steps: [
+                {
+                    step1: `Combine and mix all ingredients.`
+                },
+                {
+                    step2: `Pour over your desired meats!`
+                },
+                ]
+    },
+];
